dcee175dfa08b9888ec081054dda253ebbdcb7cd
MartinDekos <staskunm@gmail.com>
2024-11-13 20:16:36 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,87 +1,140 @@
 let duck = document.querySelector('.Ducks-Quacked')
 let parsedQuack = parseFloat(duck.innerHTML)
 
-let clickerCost = document.querySelector('.clicker-cost')
-let parsedClickerCost = parseFloat(clickerCost.innerHTML)
-let clickerlevel = document.querySelector('.clicker-level')
-let clickerincrease = document.querySelector('.clicker-increase')
-let parsedClickerIncrease = parseFloat(clickerincrease.innerHTML)
-
-
-let breadCost = document.querySelector('.bread-cost')
-let parsedBreadCost = parseFloat(breadCost.innerHTML)
-let breadlevel = document.querySelector('.bread-level')
-let breadincrease = document.querySelector('.bread-increase')
-let parsedBreadIncrease = parseFloat(breadincrease.innerHTML)
-
-
-let bakeryCost = document.querySelector('.bakery-cost')
-let parsedBakeryCost = parseFloat(bakeryCost.innerHTML)
-let bakerylevel = document.querySelector('.bakery-level')
-let bakeryincrease = document.querySelector('.bakery-increase')
-let parsedBakeryIncrease = parseFloat(bakeryincrease.innerHTML)
-
-
 let qpcText = document.getElementById("qpc-text")
 let qpsText = document.getElementById("qps-text")
+
+let DuckImgContainer = document.querySelector('.duck-img-container')
 
 let qpc = 1;
 let qps = 0;
 
-function incrementQuacks()
+const upgrades = [
+    {
+        name: 'clicker',
+        cost: document.querySelector('.clicker-cost'),
+        parsedCost: parseFloat(document.querySelector('.clicker-cost').innerHTML),
+        increase: document.querySelector('.clicker-increase'),
+        parsedIncrease: parseFloat(document.querySelector('.clicker-increase').innerHTML),
+        level: document.querySelector('.clicker-level'),
+        QuackMultiplier: 1.1,
+        CostMultiplier: 2,
+    },
+    {
+        name: 'bread',
+        cost: document.querySelector('.bread-cost'),
+        parsedCost: parseFloat(document.querySelector('.bread-cost').innerHTML),
+        increase: document.querySelector('.bread-increase'),
+        parsedIncrease: parseFloat(document.querySelector('.bread-increase').innerHTML),
+        level: document.querySelector('.bread-level'),
+        QuackMultiplier: 1.1,
+        CostMultiplier: 1.5,
+    },
+    {
+        name: 'bakery',
+        cost: document.querySelector('.bakery-cost'),
+        parsedCost: parseFloat(document.querySelector('.bakery-cost').innerHTML),
+        increase: document.querySelector('.bakery-increase'),
+        parsedIncrease: parseFloat(document.querySelector('.bakery-increase').innerHTML),
+        level: document.querySelector('.bakery-level'),
+        QuackMultiplier: 1.1,
+        CostMultiplier: 1.5,
+    },
+    {
+        name: 'feeder',
+        cost: document.querySelector('.feeder-cost'),
+        parsedCost: parseFloat(document.querySelector('.feeder-cost').innerHTML),
+        increase: document.querySelector('.feeder-increase'),
+        parsedIncrease: parseFloat(document.querySelector('.feeder-increase').innerHTML),
+        level: document.querySelector('.feeder-level'),
+        QuackMultiplier: 1.5,
+        CostMultiplier: 1.25,
+    },
+]
+
+console.log(upgrades[0].name)
+
+
+function incrementQuacks(event)
 {
     duck.innerHTML = Math.round(parsedQuack += qpc)
+
+    const x = event.offsetX
+    const y = event.offsetY
+
+    const div = document.createElement('div')
+    div.innerHTML = `+${Math.round(qpc)}` 
+    div.style.cssText = `color: white; position: absolute; top: ${y}px; left: ${x}px; font-size: 15px; pointer-events: none;`
+    DuckImgContainer.appendChild(div)
+
+    div.classList.add('fade-up')
+
+    timeout(div)
 }
 
-function buyClicker()
+const timeout = (div) => {
+    setTimeout(() => {
+        div.remove()
+    }, 800);
+}
+
+function buyUpgrade(upgrade)
 {
-    if (parsedQuack >= parsedClickerCost)
+    const matchedUpgrade = upgrades.find((u) => {
+        if (u.name === upgrade) return u
+    })
+
+    if (parsedQuack >= matchedUpgrade.parsedCost)
     {
-        duck.innerHTML = Math.round(parsedQuack -= parsedClickerCost)
+        duck.innerHTML = Math.round(parsedQuack -= matchedUpgrade.parsedCost);
+        matchedUpgrade.level.innerHTML ++
+        matchedUpgrade.parsedIncrease = parseFloat((matchedUpgrade.parsedIncrease * matchedUpgrade.QuackMultiplier).toFixed(2))
+        matchedUpgrade.increase.innerHTML = matchedUpgrade.parsedIncrease
+        matchedUpgrade.parsedCost *= matchedUpgrade.CostMultiplier;
+        matchedUpgrade.cost.innerHTML = Math.round(matchedUpgrade.parsedCost)
 
-        clickerlevel.innerHTML ++
-
-        parsedClickerIncrease = parseFloat((parsedClickerIncrease * 1.05).toFixed(2))
-        clickerincrease.innerHTML = parsedClickerIncrease
-        qpc += parsedClickerIncrease
-
-        parsedClickerCost *= 2.5
-        clickerCost.innerHTML = Math.round(parsedClickerCost)
+        if (matchedUpgrade.name === 'clicker')
+        {
+            qpc += matchedUpgrade.parsedIncrease
+        }
+        else 
+        {
+            qps += matchedUpgrade.parsedIncrease
+        }
     }
 }
 
-function buyBread()
+function save()
 {
-    if (parsedQuack >= parsedBreadCost)
-    {
-        duck.innerHTML = Math.round(parsedQuack -= parsedBreadCost)
+    localStorage.clear()
 
-        breadlevel.innerHTML ++
-
-        parsedBreadIncrease = parseFloat((parsedBreadIncrease * 1.05).toFixed(2))
-        breadincrease.innerHTML = parsedBreadIncrease
-        qps += parsedBreadIncrease
-
-        parsedBreadCost *= 1.5
-        breadCost.innerHTML = Math.round(parsedBreadCost)
-    }
+    upgrades.map((upgrade) => {
+        const obj = JSON.stringify({
+            parsedLevel: parseFloat(upgrade.level.innerHTML),
+            parsedCost: upgrade.parsedCost,
+            parsedIncrease: upgrade.parsedIncrease
+        })
+        localStorage.setItem(upgrade.name, obj)
+    })
+    localStorage.setItem('qpc', JSON.stringify(qpc))
+    localStorage.setItem('qps', JSON.stringify(qps))
+    localStorage.setItem('quacks', JSON.stringify(parsedQuack))
 }
 
-function buyBakery()
+function load()
 {
-    if (parsedQuack >= parsedBakeryCost)
-    {
-        duck.innerHTML = Math.round(parsedQuack -= parsedBakeryCost)
-
-        bakerylevel.innerHTML ++
-
-        parsedBakeryIncrease = parseFloat((parsedBakeryIncrease * 1.05).toFixed(2))
-        bakeryincrease.innerHTML = parsedBakeryIncrease
-        qps += parsedBakeryIncrease
-
-        parsedBakeryCost *= 1.5
-        bakeryCost.innerHTML = Math.round(parsedBakeryCost)
-    }
+    upgrades.map((upgrade) => {
+        const savedValues = JSON.parse(localStorage.getItem(upgrade.name))
+        upgrade.parsedCost = savedValues.parsedCost
+        upgrade.parsedIncrease = savedValues.parsedIncrease
+        upgrade.level.innerHTML = savedValues.parsedLevel
+        upgrade.cost.innerHTML = Math.round(upgrade.parsedCost)
+        upgrade.increase.innerHTML = upgrade.parsedIncrease
+    })
+    qpc = JSON.parse(localStorage.getItem('qpc'))
+    qps = JSON.parse(localStorage.getItem('qps'))
+    parsedQuack = JSON.parse(localStorage.getItem('quacks'))
+    duck.innerHTML = Math.round(parsedQuack)
 }
 
 
